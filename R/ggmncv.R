ba0e6a882223898a2551fb58cabf095288aafaed
Donald R. Williams <brycewayne03.29@gmail.com>
2020-07-02 17:28:15 -0700

    update documentation for ggmncv function

--- R/ggmncv.R
+++ R/ggmncv.R
@@ -7,7 +7,7 @@
 #'
 #' @param n Numeric. Sample size.
 #'
-#' @param lambda Numeric. Tuning parameter governing the degress of penalization. Defaults to
+#' @param lambda Numeric. Tuning parameter governing the degrees of penalization. Defaults to
 #'               \code{NULL} which results in fixing lambda to \code{sqrt(log(p)/n)}.
 #'
 #' @param n_lambda Numeric. The number of regularization/thresholding parameters
@@ -24,10 +24,10 @@
 #' @param penalty Character string. Which penalty should be used (defaults to \code{atan}).
 #'
 #' @param refit Logical. Should the precision matrix be refitted, given the adjacency matrix
-#'               (defaults to \code{FALSE})? When set to \code{TRUE}, this provides the \strong{nonregularized},
-#'               maximum likelhood estimate with constraints.
+#'               (defaults to \code{FALSE})? When set to \code{TRUE}, this provides the \strong{non-regularized},
+#'               maximum likelihood estimate with constraints.
 #'
-#' @param LLA Logical. Should the local linear approximation be used for maxmizing the penalized likelihood ?
+#' @param LLA Logical. Should the local linear approximation be used for maximizing the penalized likelihood ?
 #'            The default is \code{FALSE} (see details).
 #'
 #' @param method Character string. Which correlation coefficient should be computed.
@@ -46,8 +46,10 @@
 #' @param vip_iter Numeric. How many bootstrap sample for computing \code{vip} (defaults to 1000) ? Note
 #'                 also that only the default lambda is implemented (select is not implemented).
 #'
-
 #' @param ... Currently ignored.
+#'
+#' @references
+#' \insertAllCited{}
 #'
 #' @importFrom stats cor cov2cor
 #' @importFrom psych polychoric
@@ -58,12 +60,76 @@
 #' \itemize{
 #' \item \code{Theta} Inverse covariance matrix
 #' \item \code{Sigma} Covariance matrix
-#' \item \code{P} Weigthed adjacency matrix
+#' \item \code{P} Weighted adjacency matrix
 #' \item \code{adj} Adjacency matrix
 #' \item \code{lambda} Tuning parameter (i.e., sqrt(log(p)/n))
 #' \item \code{fit} glasso fitted model (a list)
 #' }
 #'
+#'
+#' @details Several of the penalties are (continuous) approximations to the L0 penalty, that is,
+#' best subsets model selection. However, the solution does not require enumerating
+#' all possible models which results in a computationally efficient algorithm.
+#'
+#' L0 approximations:
+#'
+#' \itemize{
+#'
+#' \item Atan: \code{penalty = "atan"} \insertCite{wang2016variable}{GGMncv}. This is currently the default.
+#'
+#' \item Seamless L0: \code{penalty = "selo"} \insertCite{dicker2013variable}{GGMncv}.
+#'
+#' \item Exponential: \code{penalty = "exp"}  \insertCite{wang2018variable}{GGMncv}
+#'
+#' \item Log: \code{penalty = "log"} \insertCite{mazumder2011sparsenet}{GGMncv}.
+#'
+#' \item SCAD: \code{penalty = "scad"}  \insertCite{fan2001variable}{GGMncv}.
+#'
+#' \item MCP: \code{penalty = "mcp"} \insertCite{zhang2010nearly}{GGMcnv}.
+#'
+#' }
+#'
+#'
+#' \strong{Gamma}
+#'
+#' The \code{code} gamma argument corresponds to additional hyperparameter for each penalty.
+#' The defaults are set to the recommended values from the respective papers.
+#'
+#' \strong{L0_learn}
+#'
+#' \code{L0_learn} is perhaps a misnomer, in that best subsets solution is not computed.
+#' This option corresponds to the following steps, assuming \code{select = TRUE}:
+#'
+#' \enumerate{
+#'
+#' \item Estimte the graph for a given lambda value
+#'
+#' \item Refit the precision matrix, given the contraints from step 1. This results
+#' in the maximum likelihood estimate (non-regularized).
+#'
+#' \item Compute BIC for the refitted graph from step 2.
+#'
+#' \item After repeating steps 1-3 for lambda value, select the graph according to BIC.
+#'
+#' }
+#'
+#' Note that this is most useful in datasets that have more nodes than variables
+#' (i.e., low-dimensional).
+#'
+#' \strong{LLA}
+#' The local linear approximate is for non-covex penalties is described in \insertCite{fan2009network}{GGMncv}.
+#' This is essentially a weighted (g)lasso. Note that by default \code{LLA = FALSE}. This is due to the
+#' work of \insertCite{zou2008one}{GGMncv}, which suggested that, so long as the starting values are good,
+#' then it is possible to use a one-step estimator. In the case of low-dimensional data , the sample based
+#' inverse covariance matrix is used to compute the lambda matrix. This is expected to work well, assuming
+#' that \emph{n} is sufficiently larger than \emph{p}. For high-dimensional data, the initial values for obtaining
+#' the lambda matrix are obtained from glasso.
+#'
+#' \strong{Model Selection}
+#'
+#' It is common to select lambda. However, in more recent approaches (see references above), lambda is fixed to
+#' \code{sqrt(log(p)/n)}. This has the advantage of being tuning free and this value is expected
+#' to provide competitive performance. It is possible to select lambda by setting \code{select = TRUE}.
 #'
 #' @examples
 #' \donttest{
@@ -102,7 +168,7 @@ GGMncv <- function(x, n,
   }
 
 
-  if (base::isSymmetric(x)) {
+  if (base::isSymmetric(as.matrix(x))) {
     R <- x
   } else {
     if (method == "polychoric") {
